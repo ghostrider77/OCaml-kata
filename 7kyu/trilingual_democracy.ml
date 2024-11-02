c37a751a26b680fe8c2c9393eb25176617aa9edf
Tamas Kurics <tamas.kurics@gmail.com>
2024-11-02 06:22:08 +0100

    fix indentation level
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- 7kyu/trilingual_democracy.ml
+++ 7kyu/trilingual_democracy.ml
@@ -1,12 +1,12 @@
 let trilingual_democracy (group : string) : char =
-  let languages = List.of_seq (String.to_seq group) in
-  match List.sort compare languages with
-    | [a; b; c] when a = b && b = c  -> a
-    | ['D'; 'D'; chr] -> chr
-    | [chr; 'K'; 'K'] -> chr
-    | ['F'; 'F'; chr] | [chr; 'F'; 'F'] | ['I'; 'I'; chr] | [chr; 'I'; 'I'] -> chr
-    | ['D'; 'F'; 'I'] -> 'K'
-    | ['D'; 'F'; 'K'] -> 'I'
-    | ['D'; 'I'; 'K'] -> 'F'
-    | ['F'; 'I'; 'K'] -> 'D'
-    | _ -> failwith "All cases should be covered."
+    let languages = List.of_seq (String.to_seq group) in
+    match List.sort compare languages with
+        | [a; b; c] when a = b && b = c -> a
+        | ['D'; 'D'; chr] -> chr
+        | [chr; 'K'; 'K'] -> chr
+        | ['F'; 'F'; chr] | [chr; 'F'; 'F'] | ['I'; 'I'; chr] | [chr; 'I'; 'I'] -> chr
+        | ['D'; 'F'; 'I'] -> 'K'
+        | ['D'; 'F'; 'K'] -> 'I'
+        | ['D'; 'I'; 'K'] -> 'F'
+        | ['F'; 'I'; 'K'] -> 'D'
+        | _ -> failwith "All cases should be covered."
